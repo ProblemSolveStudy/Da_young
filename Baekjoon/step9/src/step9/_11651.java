8d5236095a2b1fe304533f2345761b8c310bbf5c
James park <dayung0425@gmail.com>
2023-02-22 14:23:52 +0900

    0222 파일 업로드

--- Baekjoon/step9/src/step9/_11651.java
+++ Baekjoon/step9/src/step9/_11651.java
@@ -3,7 +3,7 @@ package step9;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class _11650 {
+public class _11651 {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -17,10 +17,10 @@ public class _11650 {
 		}
 		
 		Arrays.sort(arr,(a,b)->{
-			if(a[0] == b[0]) {
-				return a[1] - b[1];
-			}else {
+			if(a[1] == b[1]) {
 				return a[0] - b[0];
+			}else {
+				return a[1] - b[1];
 			}
 		});
 		
