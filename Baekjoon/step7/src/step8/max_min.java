737ef757fbc69bd24863e029b78d9cc9fc8ce023
James park <dayung0425@gmail.com>
2023-02-22 16:04:12 +0900

    일단 해본다

--- Baekjoon/step7/src/step8/max_min.java
+++
@@ -1,8 +0,0 @@
-package step8;
-
-public class max_min {
-public static void main(String[] args) {
-	System.out.println(Integer.MAX_VALUE); // 최대값
-	System.out.println(Integer.MIN_VALUE); // 최소값
-}
-}
