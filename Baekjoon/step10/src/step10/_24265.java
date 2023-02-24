5042f3c3f3a83f6c28ee63799a4eb3c6981fe0f9
James park <dayung0425@gmail.com>
2023-02-24 10:56:19 +0900

    정렬알고리즘 업로드

---
+++ Baekjoon/step10/src/step10/_24265.java
@@ -0,0 +1,15 @@
+package step10;
+
+import java.util.Scanner;
+
+public class _24265 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		
+		System.out.println(n*(n-1));
+		System.out.println("2");
+	}
+
+}
